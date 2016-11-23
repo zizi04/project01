8094ced53ec14cd17d16d0915b4694173fa6b5d2
zizi04 <disguy3939@naver.com>
2016-11-24 00:00:53 +0900

    project

--- part1-01/part1-04/04.c
+++ part1-01/part1-04/04.c
@@ -3,21 +3,24 @@
 
 int main(void)
 {
-	
-	for (int a = 0; a <= 10; a++)
+	int prime = 2,c;
+	for (int a = 1; a <=10; a++)
 	{
-		for (int b = 0; b <= (10 - a); b++)
-			printf("  ");
-		
-		for (int c = 0; c <= a; c++)
-		{
-			for (int d = 2; d<=257 ; d++)
-			{
-				
+		for (int b = 0; b < (10 - a); b++)
+			printf("  "); //9칸,8칸,7칸.....띄어쓰기
+		for (int i = 0; i < a; i++){ //1개,2개,3개....출력
+			while (1){
+				for (c = 2; prime%c != 0; c++){ //나누어 떨어지는 수를 찾을 때까지
+				}
+				if (c == prime){// 나누어 떨어지는 수가 자기 자신 뿐일때
+					printf("%3d ", prime);
+					break;
+				}
+				++prime;
 			}
-		}
-	}
-	printf("\n");
+			++prime;
+		}printf("\n");
+	}	
 }
 
 
--- part1-01/part1-05/05.c
+++ part1-01/part1-05/05.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	int data;
+	int data,n=7,hexa,flag=0;
 	printf("\n 정수를 하나 입력하세요. >> ");
 	scanf("%d", &data);
 
@@ -12,3 +12,31 @@ int main(void)
 		printf("%c", ((data >> a) & 1) ? '1' : '0');
 	printf("\n");
 
+	printf("\n8진수: ");
+	int temp = data;
+	int octal = 0;
+	int i = 1;
+	while (temp)
+	{
+		int rem = temp % 8;
+		temp /= 8;
+		octal += rem * i;
+		i *= 10;
+	}
+	printf("%32d", octal);
+	printf("%8o\n", data);
+
+	
+	
+	printf("\n");
+
+	printf("\n16진수: ");
+	printf("%30x    ", data);
+	for (unsigned int mask = 0xF0000000;mask>0;){
+		hexa = (data&mask)>>(4*n--);
+		if (hexa != 0) flag = 1;
+		if(flag) printf("%x",hexa);
+		mask = mask >> 4;
+	}
+	printf("\n");
+}
